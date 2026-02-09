6c50ba3de61691eec6b1eea57b94599c2f3949fe
Pekka Lehtonen <pekkasakari.lehtonen@gmail.com>
2026-02-09 17:48:00 +0200

    feat: add pane split horizontally
    Pane split and basic movements and pane closing

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
+
+config.enable_tab_bar = false
+config.window_background_opacity = 0.7
+
+config.color_scheme = "tokyonight-storm"
+
+-- Keybindings
+config.keys = {}
+-- Pane management
+for _, v in ipairs({
+	{"Enter", act.SplitHorizontal{domain='CurrentPaneDomain'}},
+	{"w", act.CloseCurrentPane{confirm=true}},
+	{"j", act.ActivatePaneDirection'Left'},
+	{"k", act.ActivatePaneDirection'Right'},
+}) do table.insert(config.keys, {mods="ALT", key=v[1], action=v[2]}) end
+
+return config
