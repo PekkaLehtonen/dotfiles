e0201a7a5e8ac2bac07adf598e11489dcf152c94
Pekka Lehtonen <pekkasakari.lehtonen@gmail.com>
2026-02-12 06:51:26 +0200

    feat: Add vsplit and key movements

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require("wezterm")
-local act = wezterm.action
 local config = wezterm.config_builder()
+local act = wezterm.action
 
+config.color_scheme = "tokyonight-storm"
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 16
 
-config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
 
-config.color_scheme = "tokyonight-storm"
 config.macos_window_background_blur = 15
 
 -- Keybindings
@@ -17,8 +17,12 @@ config.keys = {}
 for _, v in ipairs({
 	{"Enter", act.SplitHorizontal{domain='CurrentPaneDomain'}},
 	{"w", act.CloseCurrentPane{confirm=true}},
-	{"j", act.ActivatePaneDirection'Left'},
-	{"k", act.ActivatePaneDirection'Right'},
+	{"j", act.ActivatePaneDirection'Down'},
+	{"k", act.ActivatePaneDirection'Up'},
+	{"h", act.ActivatePaneDirection'Left'},
+	{"l", act.ActivatePaneDirection'Right'},
 }) do table.insert(config.keys, {mods="ALT", key=v[1], action=v[2]}) end
+
+table.insert(config.keys, {mods="ALT|SHIFT", key="Enter", action=act.SplitVertical{domain="CurrentPaneDomain"}})
 
 return config
