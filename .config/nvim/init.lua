6fda995c72f52a6810c1f6d944e32111cfa8a97a
Pekka Lehtonen <pekkasakari.lehtonen@gmail.com>
2026-02-19 07:56:51 +0200

    feat: modular keymaps and options
    Move keymaps and options to dedicated files
    for better maintainability

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,33 +1,5 @@
 -- global
-vim.g.mapleader = '\\'
-
+require("config.keymaps")
+require("config.options")
 require("config.lazy")
 require("lualine").setup()
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.termguicolors = true
-
--- Set line numbers
-vim.o.number = true
-
--- Work with multiple files
-vim.o.hidden = true
-
-vim.cmd("filetype plugin indent on")
-
--- Map esc to home row in insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {})
-vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {})
-
--- remap window movement without <C-w>
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true })
-
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
-
-vim.lsp.enable("lua_ls")
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- Map esc to home row in insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {})
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {})
+
+-- remap window movement without <C-w>
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><C-h>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w><C-j>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true })
+
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
