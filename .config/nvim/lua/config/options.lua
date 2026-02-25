be0bcf84d6f0e1b5c1511ed7d5e7c108080d5c56
Pekka Lehtonen <pekkasakari.lehtonen@gmail.com>
2026-02-25 17:31:51 +0200

    feat: more keymaps and options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,9 +6,46 @@ vim.o.termguicolors = true
 
 -- Set line numbers
 vim.o.number = true
+vim.o.rnu = true
 
 -- Work with multiple files
 vim.o.hidden = true
+vim.o.winborder = 'rounded'
 
-vim.lsp.enable("lua_ls")
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+
+-- mode is already visible in lualine
+vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+-- Decrease update time
+vim.o.updatetime = 250
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.wrap = false
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-guide-options`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
