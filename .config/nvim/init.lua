4bc7f93eb1be95bd8d0a97871b27b8bd533f05fb
Pekka Lehtonen <pekkasakari.lehtonen@gmail.com>
2026-02-25 17:11:13 +0200

    feat: add support for lsp and autocompletion
    Main features of lsp configuration:
      - ruff used for formatting for python files
      - ty used for diagnostics
      - keymap to format file
      - automatically detects lsp
      - uses Mason to download lsp dependencies

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,8 +16,6 @@ vim.o.number = true
 -- Work with multiple files
 vim.o.hidden = true
 
-vim.cmd("filetype plugin indent on")
-
 -- Map esc to home row in insert mode
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {})
 vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {})
@@ -29,5 +27,23 @@ vim.api.nvim_set_keymap('n', '<C-k>', '<C-w><C-k>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w><C-l>', { noremap = true })
 
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.o.winborder = 'rounded'
 
-vim.lsp.enable("lua_ls")
+-- Diagnostic Config & Keymaps
+-- See :help vim.diagnostic.Opts
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+
+  -- Can switch between these as you prefer
+  virtual_text = false, -- Text shows up at the end of the line
+  virtual_lines = true, -- Teest shows up underneath the line, with virtual lines
+
+  -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+  jump = { float = true },
+}
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
